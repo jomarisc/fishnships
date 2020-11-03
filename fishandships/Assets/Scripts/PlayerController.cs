45ed6ddeb6c55555a45ab470d966b627d80414f5
jomarisc <jomarisc@ucsc.edu>
2020-11-03 12:26:28 -0800

    Movement + Text UI
    Basic movement implemented.
    UI placeholder shows values.

---
+++ fishandships/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Controls movement in hub.
+        var movement = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(movement * moveSpeed, rb.velocity.y);
+    }
+
+}
